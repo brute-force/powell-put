629c29ec60fc992caaf0b810db5be749e5609c16
Reuel Alvarez <reuel.alvarez@gmail.com>
2022-08-06 00:03:40 -0400

    fix quote error in server'

--- server/src/app.js
+++ server/src/app.js
@@ -8,12 +8,12 @@ app.use(require('./routes/stocks'));
 app.use(require('./routes/historical'));
 
 
-if (process.env.NODE_ENV === 'production' '|| process.env.NODE_ENV === 'staging') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
- }
+}
  
 const dbo = require("./db/conn");
 
