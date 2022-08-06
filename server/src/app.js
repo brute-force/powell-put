82a3c10d1ff1b68d798414dcee9b8ae9f8045062
Reuel Alvarez <reuel.alvarez@gmail.com>
2022-08-05 23:52:47 -0400

    -fix heroku deploy

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,14 @@ app.use(express.json());
 app.use(require('./routes/stocks'));
 app.use(require('./routes/historical'));
 
+
+if (process.env.NODE_ENV === 'production' '|| process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+ }
+ 
 const dbo = require("./db/conn");
 
 module.exports = app;
