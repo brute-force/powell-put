c66be8ee37dabbcebd6769387f96704fca6eced3
Reuel Alvarez <reuel.alvarez@gmail.com>
2022-08-08 17:51:36 -0400

    yahoo ticker search

---
+++ server/src/routes/search.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routerSearch = express.Router();
+const yF = require('yahoo-finance2').default;
+
+routerSearch.route('/search').get(async (req, res) => {
+  const queryOptions = {
+    newsCount	: 0
+  };
+
+  try {
+    const searchResults = await yF.search(req.query.ticker, queryOptions);
+    const item = searchResults.quotes.find((searchResult) => searchResult.symbol === req.query.ticker.toUpperCase());
+
+    res.json(item ? [item]: []);
+  } catch (err) {
+    res.status(500);
+    res.json({ error: err.message });
+  }
+});
+
+module.exports = routerSearch;
