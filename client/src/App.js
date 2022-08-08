61385e6daafc4fac59d1a0e08ee49a18bac7f9b2
Reuel Alvarez <reuel.alvarez@gmail.com>
2022-08-07 23:07:05 -0400

    add route for fed chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Stocks from './components/stocks';
 import Chart from './components/chart';
+import Jerry from './components/jerry';
 import { Container } from '@mui/material';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Stocks />} />
           <Route exact path="/chart/:ticker" element={<Chart />} />
+          <Route exact path="/jerry/:ticker" element={<Jerry />} />
         </Routes>
       </Container>
     </div>
