bd107de8191995ff6e612335c30851a041483b7b
Reuel Alvarez <reuel.alvarez@gmail.com>
2022-08-07 23:08:44 -0400

    add hard-coded info for fomc meetings

---
+++ client/src/components/fomc.js
@@ -0,0 +1,34 @@
+const fomcMeetings = [
+  {
+    id: 1,
+    meetingStart: '01/25/22',
+    meetingEnd: '01/26/22',
+    notes: 'no hike'
+  },
+  {
+    id: 2,
+    meetingStart: '03/15/22',
+    meetingEnd: '03/16/22',
+    notes: '25 bps hike'
+  },
+  {
+    id: 3,
+    meetingStart: '05/03/22',
+    meetingEnd: '05/04/22',
+    notes: '50 bps hike; reduce holdings of Treasury securities and agency debt and agency mortgage-backed securities on June 1.'
+  },
+  {
+    id: 4,
+    meetingStart: '06/14/22',
+    meetingEnd: '06/15/22',
+    notes: '75 bps hike'
+  },
+  {
+    id: 5,
+    meetingStart: '07/26/22',
+    meetingEnd: '07/27/22',
+    notes: '75 bps hike; prepared to adjust the stance of monetary policy as appropriate if risks emerge that could impede the attainment of the Committee\'s goals.'
+  },
+];
+
+export default fomcMeetings;
